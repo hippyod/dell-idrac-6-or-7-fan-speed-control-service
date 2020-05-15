c814d4bfc92f612864c083923433ecc91dee3437
Hippy <hippyod@yahoo.com>
2020-05-14 21:38:18 -0500

    now setting fan speed across a rolling average of CPU cores taken every 30s over two minutes

--- fan-speed-control.sh
+++ fan-speed-control.sh
@@ -20,36 +20,41 @@ MIN_TEMP=25
 MAX_TEMP=$(sensors | awk '/\+[0-9][0-9]\./{ print $6; exit }' | grep -o '[0-9][0-9]')
 MAX_TEMP=$(awk "BEGIN { print ${MAX_TEMP} * 0.9 }" )
 
-TEMP_PERCENT_INCR=$(awk "BEGIN { print 100/(${MAX_TEMP} - ${MIN_TEMP}) }" )
+TEMP_PERCENT_INCR=1
+# If you want to calculate use the following
+# TEMP_PERCENT_INCR=$(awk "BEGIN { print 100/(${MAX_TEMP} - ${MIN_TEMP}) }" )
+
 printf "MAX monitoring temp set to %0.2f based on reading\n" ${MAX_TEMP}
 printf "Temperature adjustments set to %0.2f%% per °C above ${MIN_TEMP}°C\n" ${TEMP_PERCENT_INCR}
 
 # Will monitor temp every 15 seconds, and then add 15 seconds (STEP) if no change each time
 # until hitting 90 seconds.  Will reset to 15 seconds if change in temp is noticed.
-SLEEP_TIME=10
+SLEEP_TIME=30
 
+TEMP_INDEX=0
+TEMP_ARRAY=($MAX_TEMP $MIN_TEMP $MAX_TEMP $MIN_TEMP)
 while true
 do
     # Extract current temp from first core in celsius using sensors
     # command to get the temperature, and outputs it as two digits.
     TEMPS=$(sensors | awk '/\+[0-9][0-9]\./{ print $3 }' | grep -o '[0-9][0-9]' | tr '\n' ' ')
-    TEMP_TEMP=0
-    COUNT=0
-    for TEMP in ${TEMPS}
-    do
-        TEMP_TEMP=$(( ${TEMP_TEMP} + ${TEMP} ))
-        COUNT=$(( ${COUNT} + 1 ))
-    done
-    TEMP=$(echo "${TEMP_TEMP}/${COUNT}" | bc)
+    TEMP=$(echo ${TEMPS} | awk '{s=0; for (i=1;i<=NF;i++)s+=$i; print s/NF;}')
+    TEMP=$(echo "(${TEMP}+0.5)/1" | bc)
 
+    TEMP_ARRAY[$TEMP_INDEX]=${TEMP}
+
+    AVG_TEMP=$(echo ${TEMP_ARRAY[@]} | awk '{s=0; for (i=1;i<=NF;i++)s+=$i; print s/NF;}')
+    AVG_TEMP=$(echo "(${AVG_TEMP}+0.5)/1" | bc)
+
+    TEMP_INDEX=$(( (${TEMP_INDEX} + 1) % 4 ))
 
     # set control to CONTROL if within the control temp range
     CONTROL='0x01'
-    if [[ ${TEMP} < ${MAX_TEMP} ]]
+    if [[ ${AVG_TEMP} < ${MAX_TEMP} ]]
     then
         CONTROL='0x00'
     else
-        echo "Warning: Temperature has exceeded safe monitoring limits (${TEMP}°C > ${MAX_TEMP}°C)! Activating dynamic fan control!"
+        echo "Warning: Temperature has exceeded safe monitoring limits (${AVG_TEMP}°C > ${MAX_TEMP}°C)! Activating dynamic fan control!"
         TIME_STEP=6
     fi
 
@@ -59,13 +64,12 @@ do
         OLD_CONTROL=${CONTROL}
     fi
 
-    # ignored if on auto
-    SLEEP_TIME=30
+    # ignored if on auto fan control
     if [[ ${CONTROL} == '0x00' ]]
     then
-        if [[ $(( ${OLD_TEMP} + 2 )) < ${TEMP} || $(( ${OLD_TEMP} - 2 )) > ${TEMP} ]]
+        if [[ $(( ${OLD_TEMP} + 2 )) < ${AVG_TEMP} || $(( ${OLD_TEMP} - 2 )) > ${AVG_TEMP} ]]
         then
-            TEMP_PERCENT=$(awk "BEGIN { print (${TEMP} - ${MIN_TEMP}) * ${TEMP_PERCENT_INCR} }")
+            TEMP_PERCENT=$(awk "BEGIN { print (${AVG_TEMP} - ${MIN_TEMP}) * ${TEMP_PERCENT_INCR} }")
             TEMP_PERCENT=$(echo "(${TEMP_PERCENT}+0.5)/1" | bc)
 
             if [[ ${TEMP_PERCENT} < 10 ]]
@@ -74,11 +78,10 @@ do
             fi
 
             TEMP_PERCENT_HEX=$(echo "obase=16 ; ${TEMP_PERCENT}" | bc)
-            echo "Temperature reading has changed: ${OLD_TEMP}°C / new: ${TEMP}°C; Adjusting fan speed to ${TEMP_PERCENT}%"
+            echo "Temperature reading has changed: ${OLD_TEMP}°C / new: ${AVG_TEMP}°C; Adjusting fan speed to ${TEMP_PERCENT}%"
 
             ipmitool -I lanplus -H ${IPMI_HOST} -U ${IPMI_USER} -P ${IPMI_PW} raw 0x30 0x30 0x02 0xff 0x${TEMP_PERCENT_HEX}
-            OLD_TEMP=${TEMP}
-            SLEEP_TIME=15
+            OLD_TEMP=${AVG_TEMP}
         fi
     fi
 
